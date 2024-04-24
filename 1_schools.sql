3a59910c0f37ff4b671ac4f16ad99c17aa2006ba
Nekrity <149562323+Nekrity@users.noreply.github.com>
2024-04-24 09:06:08 +0300

    Update!!!

--- 1_schools.sql
+++ 1_schools.sql
@@ -102,3 +102,21 @@ INSERT INTO schools (`name`,`founder`,`class_10`,`class_11`,`class_12`) VALUES
 ,('Rīgas 31. vidusskola','Rīgas valstspilsētas pašvaldība',23,19,14)
 ,('Rīgas Kultūru vidusskola','Rīgas valstspilsētas pašvaldība',112,91,91)
 ,('Rīgas Valsts 2. ģimnāzija','Rīgas valstspilsētas pašvaldība',175,171,152);
+
+SELECT * FROM schools
+WHERE founder LIKE '%pašvaldība%';
+
+SELECT COUNT(*) FROM schools
+WHERE founder LIKE '%pašvaldība%';
+
+SELECT DISTINCT founder FROM schools;
+
+SELECT * FROM schools
+ORDER BY class_12 DESC
+LIMIT 5;
+
+SELECT * FROM schools
+ORDER BY class_10
+LIMIT 5;
+
+SELECT name, class_10 + class_11 + class_12 AS total FROM schools;
